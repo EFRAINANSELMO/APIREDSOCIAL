3eabd58fccf00334a1b68e147a8425cb6826808d
Efrain <efrainsantos30@gmail.com>
2024-11-22 08:30:31 -0600

    segundo commit

--- src/components/AppManager.js
+++ src/components/AppManager.js
@@ -2,11 +2,11 @@
 const express =require('express')
 const bodyParser =require('body-parser')
 const morgan = require('morgan');
-//const cors= require('cors');
+const cors= require('cors');
 
 // importaciones de los archivos
 const conf = require('../config/configbd.json'); //configuracion BD
-const DBManager = require('./DBManager');// Administrador de BD
+//const DBManager = require('./DBManager');// Administrador de BD
 // Rutas
 const Router = require('../routes/router');
 
@@ -32,20 +32,20 @@ class AppManager{
     }
 
     prepareService = async() =>{
-      // this.#appExpress.use(cors('origin:http://localhost:4200/'));
+       this.#appExpress.use(cors('origin:http://localhost:4200/'));
        this.#appExpress.use(express.json());
        this.#appExpress.use(bodyParser.json());
        this.#appExpress.use(bodyParser.urlencoded({ extended: true }));
        this.#appExpress.use(morgan('dev'));
-        await this.#prepareDataBase(this.#runningConfType.db);     
+     //   await this.#prepareDataBase(this.#runningConfType.db);     
         await this.#prepareRouting();
     }
-    #prepareDataBase = async (dbConfig) =>{
+   /* #prepareDataBase = async (dbConfig) =>{
         const oDBMan = new DBManager()
         await oDBMan.prepareDataBase(dbConfig);
         await usuariosModel.defineModel(oDBMan.getConnection());
          
-    }
+    }*/
    
     #prepareRouting = async () =>{
        const oRouter = new Router();
--- src/controllers/usuariosControllers.js
+++ src/controllers/usuariosControllers.js
@@ -7,13 +7,20 @@ Object.preventExtensions(this);
 }
 
 fetchUsers = async(req,resp)=>{
-    let data= await usuariosModel.fetchUsersAll();
-resp.status(200).json(data);
+  //  let data= await usuariosModel.fetchUsersAll();
+  let data="FETCH FUNCIONANDO";
+//console.log(data);
+resp.status(200).json({data});
 }
 
 createUsers = async(req,resp)=>{
-     let idrecord= await usuariosModel.createUsers(req.body);
-    resp.status(200).json(idrecord);
+    // let idrecord= await usuariosModel.createUsers(req.body);
+    //let record=req.body;
+   // console.log(req.body);
+
+//let idrecord="createUsers funcionando";
+//console.log(idrecord);
+ resp.status(200).json(req.body);
 }
 
 updateUsers = async()=>{
